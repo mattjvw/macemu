a84f273d978c730e5fca7b83b660ee1e4495a4a8
kanjitalk755 <kanjitalk755@yahoo.co.jp>
2021-02-11 14:04:41 +0900

    test full screen mode for macOS

--- BasiliskII/src/SDL/video_sdl2.cpp
+++ BasiliskII/src/SDL/video_sdl2.cpp
@@ -737,11 +737,7 @@ static SDL_Surface * init_sdl_video(int width, int height, int bpp, Uint32 flags
 			shutdown_sdl_video();
 			return NULL;
 		}
-#ifdef __MACOSX__
-		window_flags |= SDL_WINDOW_FULLSCREEN_DESKTOP;
-#else
 		window_flags |= SDL_WINDOW_FULLSCREEN;
-#endif
 		window_width = desktop_mode.w;
 		window_height = desktop_mode.h;
 	}
